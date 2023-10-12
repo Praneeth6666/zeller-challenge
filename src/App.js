7b85ca69e88afc27abefbed4b02666eddbdf4036
praneeth <praneeth.koripalli@gmail.com>
2023-10-12 20:21:50 +1100

    Firat Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ListZellerCustomers } from "./queries";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import styled from "styled-components";
+import awsconfig from "./aws-exports";
+import { useState } from "react";
+Amplify.configure(awsconfig);
+
+const {
+  data: {
+    listZellerCustomers: { items: items },
+  },
+} = await API.graphql(graphqlOperation(ListZellerCustomers));
+
+const userTypes = [
+  { name: "ADMIN", id: "ADMIN", value: "ADMIN", label: "Admin" },
+  { name: "MANAGER", id: "MANAGER", value: "MANAGER", label: "Manager" },
+];
+
+function titleCase(string) {
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+const UserTypeLabel = styled.label`
+  display: inline-block;
+  margin: 2px;
+  padding: 0.7em 1em;
+  background-color: #f0f8ff;
+  border-radius: 5px;
+  width: 80%;
+`;
+
+const UserListAlpha = styled.div`
+  padding: 15px 20px 15px 20px;
+  background-color: #f0f8ff;
+  color: #1e90ff;
+  border-radius: 5px;
+`;
+
+const UserListLabel = styled.div`
+  display: flex;
+  width: 80%;
+  flex-direction: column;
+  padding-left: 1%;
+`;
+
+const UserListWrap = styled.div`
+  display: flex;
+  width: 80%;
+  flex-direction: row;
+  padding: 7px;
+`;
 
 function App() {
+  const [selectState, setSelectState] = useState("ADMIN");
+  const handlechange = (e) => {
+    const { name } = e.target;
+    setSelectState(name);
+  };
+
+  const UserTypeSelect = ({ name, value, label }) => (
+    <UserTypeLabel
+      name={name}
+      style={selectState !== name ? { backgroundColor: "white" } : {}}
+    >
+      <input
+        type="radio"
+        name={name}
+        value={value}
+        onChange={handlechange}
+        checked={value === selectState}
+      />
+      <span style={{ paddingLeft: "4px" }}>{label}</span>
+    </UserTypeLabel>
+  );
+
+  const UserListItem = ({ name, role }) => (
+    <>
+      <UserListWrap>
+        <UserListAlpha>{name[0]}</UserListAlpha>
+        <UserListLabel>
+          <div style={{ paddingBottom: "10px" }}> {name}</div>
+          <div>{role}</div>
+        </UserListLabel>
+      </UserListWrap>
+    </>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>User Types</h2>
+      {userTypes.map(({ name, id, value, label }) => (
+        <UserTypeSelect name={name} value={value} label={label} key={id} />
+      ))}
+      <h2>{titleCase(selectState)} Users</h2>
+      {items.map(({ id, name, role }) => {
+        return (
+          <div key={id}>
+            {selectState == role ? (
+              <UserListItem name={name} role={role} />
+            ) : (
+              <></>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/queries.js
@@ -0,0 +1,14 @@
+
+export const ListZellerCustomers = /* GraphQL */`
+  query ListZellerCustomers {
+  listZellerCustomers {
+    items {
+      email
+      id
+      name
+      role
+    }
+  }
+}
+`
+
